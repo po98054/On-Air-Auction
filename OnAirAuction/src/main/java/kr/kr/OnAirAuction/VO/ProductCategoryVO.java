a96cc19ad3b24e2fab9d13b60967ed4fc4998a3e
lim-ingyu <116549023+lim-ingyu@users.noreply.github.com>
2023-04-24 21:18:35 +0900

    Add files via upload

---
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO/ProductCategoryVO.java
@@ -0,0 +1,12 @@
+package kr.kh.onAirAuction.vo;
+
+import lombok.Data;
+
+@Data
+public class ProductCategoryVO {
+	
+	int pc_num;
+	String pc_name;
+	int pc_psc_num;
+	
+}
---
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO/ProductVO.java
@@ -0,0 +1,26 @@
+package kr.kh.onAirAuction.vo;
+
+import lombok.Data;
+
+@Data
+public class ProductVO {
+	
+	int pr_code;
+	String pr_name;
+	String pr_intro;
+	String pr_condition;
+	String pr_detail;
+	String pr_location;
+	String pr_refund_ok;
+	String pr_refunddate;
+	String pr_refundreason; 
+	String pr_state;
+	int pr_startprice;
+	String pr_origin; 
+	int pr_size;
+	int pr_st_num;
+	int pr_pc_num;
+	
+	String pc_name;
+	
+}
