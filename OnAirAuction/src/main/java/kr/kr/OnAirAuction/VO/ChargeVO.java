7ee5a12b915b6956604b93c02de73213f2c19053
lim-ingyu <116549023+lim-ingyu@users.noreply.github.com>
2023-05-02 15:41:54 +0900

    Add files via upload

---
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO/ChargeVO.java
@@ -0,0 +1,14 @@
+package kr.kh.onAirAuction.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChargeVO {
+	
+	int ch_num;
+	int ch_amount;
+	String ch_method;
+
+}
