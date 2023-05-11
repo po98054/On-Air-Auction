26ec22bf5b3bb70010f65b5e8985ec1eb01bbed7
lim-ingyu <116549023+lim-ingyu@users.noreply.github.com>
2023-05-11 20:03:17 +0900

    Update ProductService.java

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/Service/ProductService.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/Service/ProductService.java
@@ -1,18 +1,28 @@
 package kr.kh.onAirAuction.service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.onAirAuction.pagination.Criteria;
+import kr.kh.onAirAuction.vo.AuctionTypeVO;
+import kr.kh.onAirAuction.vo.AuctionVO;
+import kr.kh.onAirAuction.vo.ChargeVO;
 import kr.kh.onAirAuction.vo.FileVO;
+import kr.kh.onAirAuction.vo.MemberVO;
 import kr.kh.onAirAuction.vo.ProductCategoryVO;
+import kr.kh.onAirAuction.vo.ProductLargeCategoryVO;
+import kr.kh.onAirAuction.vo.ProductMiddleCategoryVO;
+import kr.kh.onAirAuction.vo.ProductSmallCategoryVO;
 import kr.kh.onAirAuction.vo.ProductVO;
 
 public interface ProductService {
+	
 	// 상품 등록
-	void insert(ProductCategoryVO pc, ProductVO vo, MultipartFile[] files);
-
+	void insertProduct(ProductLargeCategoryVO plc, ProductMiddleCategoryVO pmc, ProductSmallCategoryVO psc,
+			ProductCategoryVO pc, ProductVO vo, MultipartFile[] files);
+	
 	// 상품 리스트
 	ArrayList<ProductVO> getProductList(Criteria cri);
 
@@ -28,5 +38,9 @@ public interface ProductService {
 	
 	// 상품 수정
 	boolean updateProduct(ProductVO vo, MultipartFile[] files, int[] fileNums);
-	
+
+	boolean loginMember(MemberVO member);
+
+
+
 }
