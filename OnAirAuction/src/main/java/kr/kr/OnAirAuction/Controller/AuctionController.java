290625db60b6c4642b427660b06e8b854cc4bdcd
lim-ingyu <116549023+lim-ingyu@users.noreply.github.com>
2023-05-11 21:15:23 +0900

    Update AuctionController.java
    detail  추가

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/Controller/AuctionController.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/Controller/AuctionController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +14,8 @@ import kr.kh.onAirAuction.pagination.PageMaker;
 import kr.kh.onAirAuction.service.AuctionService;
 import kr.kh.onAirAuction.vo.AuctionTypeVO;
 import kr.kh.onAirAuction.vo.AuctionVO;
+import kr.kh.onAirAuction.vo.FileVO;
+import kr.kh.onAirAuction.vo.ProductVO;
 
 @Controller
 public class AuctionController {
@@ -20,28 +23,46 @@ public class AuctionController {
 	@Autowired
 	private AuctionService auctionservice;
 	
-	@RequestMapping(value = "/auction")
-	public ModelAndView product(ModelAndView mv) {		
+	//경매 등록
+	@RequestMapping(value="/auction/insert", method=RequestMethod.GET)
+	public ModelAndView product(ModelAndView mv) {
 		mv.setViewName("/auction/insert");	
 		return mv;
 	}
 	
-	//경매 등록
     @RequestMapping(value="/auction/insert", method=RequestMethod.POST)
-	public String insertAuction(AuctionVO auction){
+	public ModelAndView insertAuctionPOST(ModelAndView mv, AuctionVO auction){
     	auctionservice.insertAuction(auction);
-		return "redirect:/auction/list";
+    	mv.setViewName("redirect:/auction/list");
+		return mv;
 	}
     
-    // 상품 리스트
+    // 경매 등록 리스트
     @RequestMapping(value="/auction/list", method=RequestMethod.GET)
-    public ModelAndView auctionList(ModelAndView mv, Criteria cri, AuctionTypeVO ac_name) {
+    public ModelAndView auctionList(ModelAndView mv, Criteria cri) {
     	ArrayList<AuctionVO> list = auctionservice.getAuctionList(cri);
-		int totalCount = auctionservice.getBoardTotalCount(cri);
+		int totalCount = auctionservice.getAuctionTotalCount(cri);
 		PageMaker pm = new PageMaker(totalCount, 3, cri);
 		mv.addObject("list",list);
 		mv.addObject("pm", pm);
 		mv.setViewName("/auction/list");
 		return mv;
 	}
+    
+    //경매 상세 페이지
+    @RequestMapping(value="/auction/detail/{au_num}", method=RequestMethod.GET)
+    public ModelAndView  auctionDetail(ModelAndView mv, 
+    		@PathVariable("au_num") int au_num) {
+    	AuctionVO vo = auctionservice.getAuction(au_num);
+    	mv.setViewName("/auction/detail");
+    	 if(vo.getAu_ac_num() == 1) {
+    	        mv.setViewName("/auctioncategory/general");
+    	    } else if(vo.getAu_ac_num() == 2) {
+    	        mv.setViewName("/auctioncategory/onair");
+    	    }
+    	 
+    	return mv;
+    }
+        
+    
 }
