650e6a39e253afc64d6f839c39ee686216c65e6b
lim-ingyu <116549023+lim-ingyu@users.noreply.github.com>
2023-05-12 15:52:18 +0900

    Update ProductVO.java

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO2(전체VO클래스)/ProductVO.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO2(전체VO클래스)/ProductVO.java
@@ -21,6 +21,35 @@ public class ProductVO {
 	String pr_size;
 	int pr_st_num;
 	int pr_pc_num;
+	
+	int st_num;
+	
+	//productcategory
+	int pc_num;
+	String pc_name;
+	int pc_psc_num;
+	//productsmallcategory
+	int psc_num;
+	String psc_name;
+	int psc_pm_num;
+	//productmiddlecategory
+	int pmc_num;
+	String pmc_name;
+	int pmc_plc_num;
+	//productlargecategory
+	int plc_num;
+	String plc_name;
+	
+	@Override
+	public String toString() {
+		
+		return " 상품 코드 : " + pr_code + " 상품명 : " + pr_name + " 상품소개 : " + pr_intro + " 상품상태 : " + pr_condition + " 상세정보 : " + 	pr_detail
+				
+				+ " 상품 위치 : " + pr_location + " 반품수용여부 : " +  pr_refund_ok + " 반품수용기간 : " + pr_refund_date + " 반품가능조건 : " + pr_refund_reason
+				
+				+ " 경매시작가격: " + pr_start_price + " 원산지 : " +  pr_origin + " 상품크기 : " +  pr_size + " 스토어번호 : " +  pr_st_num + " 세분류번호 : " +  pr_pc_num; 
+				
+	
 }
 	
 	
