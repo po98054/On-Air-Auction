048893cdd90a33a1be081cde03e0404bb67cba71
imradev <110815222+imradev@users.noreply.github.com>
2023-05-15 11:41:26 +0900

    Update GeneralServiceImp.java

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/Service/GeneralServiceImp.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/Service/GeneralServiceImp.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Service;
 
 import kr.kr.OnAirAuction.DAO.GeneralDAO;
 import kr.kr.OnAirAuction.Pagination.Criteria;
+import kr.kr.OnAirAuction.VO.AuctionRecordVO;
 import kr.kr.OnAirAuction.VO.AuctionVO;
 import kr.kr.OnAirAuction.VO.MemberVO;
-import kr.kr.OnAirAuction.VO.ProductVO;
-//import kr.kr.OnAirAuction.VO.Product_categoryVO;
+import kr.kr.OnAirAuction.VO.ProdCategoryVO;
 import kr.kr.OnAirAuction.VO.ProductLikeVO;
+import kr.kr.OnAirAuction.VO.ProductVO;
 import kr.kr.OnAirAuction.VO.SellerLikeVO;
 import kr.kr.OnAirAuction.VO.StoreVO;
 
@@ -36,23 +37,33 @@ public class GeneralServiceImp implements GeneralService{
 	    return product;
 	}
 	
-//	// 일반 경매 상품 카테고리
-//	@Override
-//	public Product_categoryVO getProdCategory(int pr_code) {
-//		ProductVO product = generalDao.selectGeneral(pr_code);
-//		// 카테고리 가져오기
-//		Product_categoryVO prodCategory = generalDao.selectProdCategory(product.getPr_pc_num());
-//		return prodCategory;
-//	}
-	
+	// 일반 경매 상품 카테고리
 	@Override
-	public int getGeneralTotalCount(Criteria cri) {
-		cri = cri == null ? new Criteria() : cri;
-		return generalDao.selectTotalCountProduct(cri);
+	public ProdCategoryVO getProdCategory(int pr_code) {
+		ProductVO product = generalDao.selectGeneral(pr_code);
+		// 카테고리 가져오기
+		ProdCategoryVO prodCategory = generalDao.selectProdCategory(product.getPr_pc_num());
+		return prodCategory;
 	}
 	
+	// 경매정보 가져오기
+	@Override
+	public AuctionVO getAuction(int pr_code) {
+		AuctionVO auction = generalDao.selectAuction(pr_code);
+		return auction;
+	}
+
+	// 스토어 정보 가져오기
+	@Override
+	public StoreVO getStore(int pr_code) {
+		StoreVO store = generalDao.selectStore(pr_code);
+		return store;
+	}
+	
+	// -------- 추가중 ----------
+	
 	// 상품 찜하기
-	/*@Override
+	@Override
 	public int updateLikeProduct(int pl_pr_code, int pl_state, MemberVO user) {
 		// 기존의 찜의 유무 정보를 가져옴
 		ProductLikeVO pLikeVo = generalDao.selectLikeProductById(user.getMe_id(), pl_pr_code);
@@ -77,10 +88,44 @@ public class GeneralServiceImp implements GeneralService{
 			generalDao.updateLikeProduct(pLikeVo);
 			// 0을 리턴
 			return 0;
-	}*/
+	}
+	
+	
+	
+	// 상세페이지 상품 좋아요 가져오기
+	@Override
+	public ProductLikeVO getLikeProduct(MemberVO user, int pr_code) {
+		if(user == null)
+			return null;
+		return generalDao.selectLikeProductById(user.getMe_id(), pr_code);
+	}	
+	
+	@Override
+	public boolean insertImmediate(MemberVO user, AuctionVO bid) {
+		if(user == null)
+			return false;
+		//bid.setAr_me_id(user.getMe_id());
+		generalDao.insertImmediate(bid);
+		return true;
+	}
+	
+	@Override
+	public ArrayList<AuctionRecordVO> getBidlist(Criteria cri, int pr_code) {
+		ProductVO product = generalDao.selectGeneral(pr_code);
+		if(cri == null)
+			cri = new Criteria();
+		return generalDao.selectBidlist(cri);
+	}
+
+	@Override
+	public int getBidlistTotalCount(Criteria cri) {
+		return generalDao.selectBidlistTotalCount(cri);
+	}
+	
+	
 	
 	// 판매자 찜하기
-	/*@Override
+	@Override
 	public int updateLikeSeller(String sl_seller_id, int sl_state, MemberVO user) {
 		// 기존의 찜의 유무 정보를 가져옴
 		SellerLikeVO sLikeVo = generalDao.selectLikeSellerById(user.getMe_id(), sl_seller_id);
@@ -105,29 +150,27 @@ public class GeneralServiceImp implements GeneralService{
 			generalDao.updateLikeSeller(sLikeVo);
 			// 0을 리턴
 			return 0;
-	}*/
-
-	// 상세페이지 상품 좋아요 가져오기
-	@Override
-	public ProductLikeVO getLikeProduct(MemberVO user, int pr_code) {
-		if(user == null)
-			return null;
-		return generalDao.selectLikeProductById(user.getMe_id(), pr_code);
-	}
-
-	// 경매정보 가져오기
-	@Override
-	public AuctionVO getAuction(int pr_code) {
-		AuctionVO auction = generalDao.selectAuction(pr_code);
-		return auction;
-	}
-
-	// 스토어 정보 가져오기
-	@Override
-	public StoreVO getStore(int pr_code) {
-		StoreVO store = generalDao.selectStore(pr_code);
-		return store;
 	}
 	
+	@Override
+	public int getGeneralTotalCount(Criteria cri) {
+		cri = cri == null ? new Criteria() : cri;
+		return generalDao.selectTotalCountProduct(cri);
+	}
+	
+	// 경매 기록 가져오기
+	@Override
+	public AuctionRecordVO getAuctionRecord(int pr_code) {
+		AuctionRecordVO auctionRecord = generalDao.selectAuctionRecord(pr_code);
+			return auctionRecord;
+	}
+
+	@Override
+	public boolean insertBid(MemberVO user, Double price, AuctionRecordVO auRecord, int auctionNum) {
+		String id = user.getMe_id();
+		generalDao.insertAuctionRecord(price, id, auctionNum);
+				
+		return false;
+	}
 
 }
