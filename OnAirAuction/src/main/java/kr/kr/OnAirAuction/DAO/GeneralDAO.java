e9fdcfe9b48ebd8d4cfe916b7691d14feb12513e
imradev <110815222+imradev@users.noreply.github.com>
2023-05-15 11:40:52 +0900

    Update GeneralDAO.java

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/DAO/GeneralDAO.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/DAO/GeneralDAO.java
@@ -6,9 +6,9 @@ import org.apache.ibatis.annotations.Param;
 
 import kr.kr.OnAirAuction.Pagination.Criteria;
 import kr.kr.OnAirAuction.VO.AuctionVO;
-//import kr.kr.OnAirAuction.VO.ProdCategoryVO;
+import kr.kr.OnAirAuction.VO.AuctionRecordVO;
+import kr.kr.OnAirAuction.VO.ProdCategoryVO;
 import kr.kr.OnAirAuction.VO.ProductVO;
-//import kr.kr.OnAirAuction.VO.Product_categoryVO;
 import kr.kr.OnAirAuction.VO.ProductLikeVO;
 import kr.kr.OnAirAuction.VO.SellerLikeVO;
 import kr.kr.OnAirAuction.VO.StoreVO;
@@ -21,9 +21,8 @@ public interface GeneralDAO {
 	// 상품 상세페이지 가져오기
 	ProductVO selectGeneral(@Param("pr_code")int pr_code);
 
-	// 오류 발생 수정 필요
 	// 상품 카테고리 가져오기
-	// Product_categoryVO selectProdCategory(@Param("pc_num")int pc_num);
+	ProdCategoryVO selectProdCategory(@Param("pc_num")int pc_num);
 
 	// 상품의 경매 정보 가져오기
 	AuctionVO selectAuction(@Param("pr_code")int pr_code);
@@ -31,12 +30,19 @@ public interface GeneralDAO {
 	// 회원의 찜한 상품 정보 가져오기
 	ProductLikeVO selectLikeProductById(@Param("pl_me_id")String pl_me_id, @Param("pl_pr_code")int pl_pr_code);
 
-	// 상품 찜하기 등록
-	void insertLikeProduct(@Param("p_li")ProductLikeVO product_likeVO);
+	// 스토어 정보
+	StoreVO selectStore(@Param("pr_code")int pr_code);
 
-	// 상품 찜하기 변경(수정 필요)
-	void updateLikeProduct(@Param("p_li")ProductLikeVO product_likeVO);
 	
+	
+	/* 추가중 */
+	
+	// 상품 찜하기 등록
+	void insertLikeProduct(@Param("p_li")ProductLikeVO pLikeVo);
+	
+	// 상품 찜하기 변경(수정 필요)
+	void updateLikeProduct(@Param("p_li")ProductLikeVO pLikeVo);
+
 	int selectTotalCountProduct(@Param("cri")Criteria cri);
 
 	// 회원의 찜한 판매자 정보 가져오기
@@ -48,8 +54,20 @@ public interface GeneralDAO {
 	// 판매자 찜하기 변경(수정 필요)
 	void updateLikeSeller(@Param("s_li")SellerLikeVO seller_likeVO);
 	
-	// 스토어 정보
-	StoreVO selectStore(@Param("pr_code")int pr_code);
+	// 경매 기록 가져오기
+	ArrayList<AuctionRecordVO> selectBidlist(@Param("cri") Criteria cri);  // ?
+
+	int insertImmediate(@Param("bid") AuctionVO bid);
+	
+	int selectBidlistTotalCount(@Param("cri") Criteria cri); // ? 
 
 
+	void insertAuctionRecord(@Param("price")Double price, @Param("id")String id, @Param("auNum")int auctionNum);
+
+	int selectBidCount(@Param("pr_code")int pr_code);
+
+	// 경매 기록 가져오기
+	AuctionRecordVO selectAuctionRecord(@Param("pr_code")int pr_code);
+
+	
 }
