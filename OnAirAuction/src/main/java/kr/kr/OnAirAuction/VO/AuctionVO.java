9ff3b6a85c9860edf7c5fcf29e9cf693ad525427
lim-ingyu <116549023+lim-ingyu@users.noreply.github.com>
2023-05-17 18:12:31 +0900

    Update AuctionVO.java

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO/AuctionVO.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/VO/AuctionVO.java
@@ -11,20 +11,47 @@ public class AuctionVO {
 	
 	int au_num;
 	int au_calculation;
-	String au_startdate;
-	String au_finaldate;
+	public Date au_start_date;
+	public Date au_final_date;
 	int au_extension;
 	int au_faild;
 	int au_immediate;
-	String au_limitbidtime;	
-	String au_limitlevel;
-	String au_deliveryway;
+	int au_limit_bid_time;	
+	int au_limit_level;
+	String au_delivery_way;
 	int au_pr_code;
 	String au_me_id;
 	int au_ac_num;
 	
 	int ac_num;
 	String ac_name;
+	
+	public String getAu_start_date() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        if (au_start_date == null) {
+            return null;
+        }
+        return dateFormat.format(au_start_date);
+    }
+	
+	public void setAu_start_date(String au_start_date) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        this.au_start_date = dateFormat.parse(au_start_date);
+    }
+	
+    public String getAu_final_date() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        if (au_final_date == null) {
+            return null;
+        }
+        return dateFormat.format(au_final_date);
+    }
+
+    public void setAu_final_date(String au_final_date) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        this.au_final_date = dateFormat.parse(au_final_date);
+    }
+
 	
 
 }
