a4873f9a089a27dd4d2b37ba14cbbf0fb3f0884d
imradev <110815222+imradev@users.noreply.github.com>
2023-05-22 11:33:23 +0900

    Update GeneralService.java

--- OnAirAuction/src/main/java/kr/kr/OnAirAuction/Service/GeneralService.java
+++ OnAirAuction/src/main/java/kr/kr/OnAirAuction/Service/GeneralService.java
@@ -3,20 +3,26 @@ package kr.kr.OnAirAuction.Service;
 import java.util.ArrayList;
 
 import kr.kr.OnAirAuction.Pagination.Criteria;
-import kr.kr.OnAirAuction.VO.AuctionVO;
 import kr.kr.OnAirAuction.VO.AuctionRecordVO;
+import kr.kr.OnAirAuction.VO.AuctionVO;
+import kr.kr.OnAirAuction.VO.FileVO;
+import kr.kr.OnAirAuction.VO.InquiryCategoryVO;
+import kr.kr.OnAirAuction.VO.InquiryVO;
 import kr.kr.OnAirAuction.VO.MemberVO;
+import kr.kr.OnAirAuction.VO.MembershipLevelVO;
 import kr.kr.OnAirAuction.VO.ProdCategoryVO;
-import kr.kr.OnAirAuction.VO.ProductVO;
 import kr.kr.OnAirAuction.VO.ProductLikeVO;
+import kr.kr.OnAirAuction.VO.ProductVO;
+import kr.kr.OnAirAuction.VO.ReviewVO;
 import kr.kr.OnAirAuction.VO.StoreVO;
+import kr.kr.OnAirAuction.VO.VirtualAccountVO;
 
 public interface GeneralService {
 	
 	// 일반 경매 목록
 	ArrayList<ProductVO> getGeneralList(Criteria cri);
-
-	// 수정 필요!!!
+	
+	// 일반 경매의 전체의 수
 	int getGeneralTotalCount(Criteria cri);
 	
 	// 일반 경매 상품
@@ -30,35 +36,41 @@ public interface GeneralService {
 	
 	// 스토어 정보
 	StoreVO getStore(int pr_code);
-		
-	// 상품 찜하기
-	int updateLikeProduct(int pl_pr_code, int pl_state, MemberVO user);
+	
+	// 경매 기록
+	ArrayList<AuctionRecordVO> getAuctionRecord(int pr_code);
+	
+	// 경매 최근 기록
+	ArrayList<AuctionRecordVO> selectAuctionRecordList();
+	
+	// 이미지 파일
+	ArrayList<FileVO> getProdFileList(int pr_code);
+	
+	// 상품 후기
+	ArrayList<ReviewVO> getReview(int pr_code);
+	
+	// 입찰하기: 가상계좌
+	VirtualAccountVO getUserAccount(String id);
+	
+	// 입찰하기: 회원등급
+	MembershipLevelVO getMembership(String memberShipName);
+	
+	// 입찰하기: 입찰 등록
+	boolean insertBid(int price, MemberVO user, VirtualAccountVO userAccount, int expense, int auctionNum);
+	
+	
+	
+	// ---------------------------------------------------------------------------------
+	
 
-	// 판매자 찜하기
-	int updateLikeSeller(String sl_seller_id, int sl_state, MemberVO user);
-	
-	// 상세페이지 상품 좋아요 가져오기
-	ProductLikeVO getLikeProduct(MemberVO user, int pr_code);
-	
-	
-	// ---- 추가중 -----
-	
-	
-	boolean insertImmediate(MemberVO user, AuctionVO bid);
-	
-	ArrayList<AuctionRecordVO> getBidlist(Criteria cri, int pr_code);
+	// 샹품 찜 구현중
+	ProductLikeVO getProdLike(MemberVO user, int pr_code);
 
-	int getBidlistTotalCount(Criteria cri);
+	// 상품 문의 구현중
+	ArrayList<InquiryVO> getInquiry(int pr_code);
 
+	// 상품 문의 등록 구현중
+	boolean insertInquiry(InquiryVO inquiry);
 	
-	
-	// 구현중
-	// 입찰하기 금액 추가
-	boolean insertBid(MemberVO user, Double price, AuctionRecordVO auRecord, int auctionNum);
-
-	
-	// 구현중
-	// 경매기록 가져오기
-	AuctionRecordVO getAuctionRecord(int pr_code);
 
 }
